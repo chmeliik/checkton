7169486a7f5e30067199823820cead9c94f85c16
Adam Cmiel <acmiel@redhat.com>
2024-05-31 15:07:21 +0200

    Always mark repo directory as safe
    To avoid
    
    fatal: detected dubious ownership in repository at '/github/workspace'
    To add an exception for this directory, call:
    	git config --global --add safe.directory /github/workspace
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- src/action.sh
+++ src/action.sh
@@ -3,4 +3,5 @@ set -o errexit -o nounset -o pipefail
 
 SCRIPTDIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
+git config --global --add safe.directory "$PWD"
 "$SCRIPTDIR"/differential-checkton.sh | csgrep --embed-context 4
