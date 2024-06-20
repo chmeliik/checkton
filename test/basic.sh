c06b186be180314635879547c3f4838896632497
Adam Cmiel <acmiel@redhat.com>
2024-06-20 15:44:14 +0200

    Refactor basic tests
    Add a function that tests everything we want to test
    
    Move extracted scripts to a more predictable directory path
    Move linter outputs to a more predictable directory path
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- test/basic.sh
+++ test/basic.sh
@@ -11,19 +11,21 @@ checkton_jq() {
     checkton.py "$@" | jq
 }
 
-@test "extract scripts from a Tekton YAML" {
-    run checkton.py --scriptdir "$BATS_TEST_TMPDIR" "$R/tektontask/tektontask.yaml"
-    run ls -1 "$BATS_TEST_TMPDIR/test/resources/tektontask/tektontask.yaml/"
-    assert_output "$(printf '%d.sh\n' {0..4})"
-    for i in {0..4}; do
-        run cat "$BATS_TEST_TMPDIR/$R/tektontask/tektontask.yaml/${i}.sh"
-        assert_output_file "$output" "$R/tektontask/extracted-scripts/${i}.sh"
+test_basic_checkton() {
+    local file_to_check=$1
+
+    run checkton_jq --scriptdir "$BATS_TEST_TMPDIR" "$file_to_check"
+    local checkton_output="$output"
+    assert_output_file "$checkton_output" "${file_to_check}-outputs/shellcheck.json"
+    test_human_readable_files "$checkton_output" "${file_to_check}-outputs"
+
+    find "$BATS_TEST_TMPDIR/$file_to_check/" -type f | while read -r script_file; do
+        run cat "$script_file"
+        assert_output_file "$output" "${file_to_check}-scripts/$(basename "$script_file")"
     done
+
 }
 
 @test "checkton a Tekton YAML" {
-    run checkton_jq "$R/tektontask/tektontask.yaml"
-    local checkton_output="$output"
-    assert_output_file "$checkton_output" "$R/tektontask/shellcheck.json"
-    test_human_readable_files "$checkton_output" "$R/tektontask"
+    test_basic_checkton "$R/tektontask/tektontask.yaml"
 }
